c8fbc22855cdc0f1f07dbc8a847a3213ddf24ef6
Ali Ma <azym1025@gmail.com>
2021-02-19 17:53:54 -0800

    Added Logout feature and dismiss it to Login Page

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeTableViewController.swift
+//  AliTwitter
+//
+//  Created by Ali Ma on 2/19/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    @IBAction func LogoutButton(_ sender: Any) {
+        
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+
+}
