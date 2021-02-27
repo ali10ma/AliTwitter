adf354b084cb3353a31373ad1d4e94f1f31def76
Ali Ma <azym1025@gmail.com>
2021-02-26 19:28:26 -0800

    Added features: Users can create tweets through AliTwitter

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -24,7 +24,14 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.loadTweets()
         
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     @objc func loadTweets(){
